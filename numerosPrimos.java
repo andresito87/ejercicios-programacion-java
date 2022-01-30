601a2a086ebbc5a7ca3e25aa15c6f1f4d228a610
andres <andrespodadera87@gmail.com>
2022-01-31 00:22:22 +0100

    funcion terminada

--- numerosPrimos.java
+++ numerosPrimos.java
@@ -1,12 +1,32 @@
+import javax.swing.JOptionPane;
 
 public class numerosPrimos {
 
 public static void main(String[] args) {
-    
-    System.out.println("Hola mundo");
 
-    System.out.println("Hola mundo");
-    System.out.println("Hola mundo");
+    int numero=Integer.parseInt(JOptionPane.showInputDialog("Introduce numeo entero"));
+    
+    esPrimo(numero);
+
 }
+ public static boolean esPrimo(int numero){   
+    if (numero<=1){
+        System.out.println("El numero "+numero+" es negativo");
+        return false;
+    }
+    for(int i=(int)Math.sqrt(numero);i>1;i--){  /*Se puede empezar 
+        //desde la raiz cuadrada de ese numero(pequeño tip para
+        // numeros primos)*/
+
+        if(numero%i==0){
+            System.out.println("El numero "+numero+" no es primo");
+            return false;
+
+        }
+        
+    }
+    System.out.println("El numero "+numero+" es primo");
+    return true;
+ }
 
 }
