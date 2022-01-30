422b15cc38ae54428c0eb1fdeb914ca953cd1b77
andres <andrespodadera87@gmail.com>
2022-01-30 23:32:58 +0100

    agrego sys

--- numerosPrimos.java
+++ numerosPrimos.java
@@ -4,7 +4,9 @@ public class numerosPrimos {
 public static void main(String[] args) {
     
     System.out.println("Hola mundo");
-  
+
+    System.out.println("Hola mundo");
+    System.out.println("Hola mundo");
 }
 
 }
